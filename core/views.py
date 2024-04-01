8d575b9d156f19701070185ac7762790fa8e7ef4
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-04-01 23:45:41 +0200

    wip on views

--- core/views.py
+++ core/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Course, Module
 
 # Create your views here.
+
+
+def courses(request):
+    courses = Course.objects.all()
+    context = {
+        'courses': courses
+    }
+    return render(request, 'university.html', context)
