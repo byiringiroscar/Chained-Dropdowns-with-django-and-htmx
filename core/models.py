0d320dff4231abf079230b4b1f74a3fe65322004
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-04-01 23:42:37 +0200

    load sample data

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Module(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='modules')
+    name = models.CharField(max_length=100)
